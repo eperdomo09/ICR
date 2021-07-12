5dc86046527abfc1841f91fa0621262d3e141f13
eperdomo09 <eraldoepl@gmail.com>
2021-07-12 17:20:27 -0400

    creating home page.

---
+++ js/main.js
@@ -0,0 +1,64 @@
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
+);
+
+if (window.screen.width >= 1440) {
+  // Slideshow or Carousel
+  var slideIndex = 1;
+  showSlides(slideIndex);
+
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides((slideIndex += n));
+  }
+
+  // Thumbnail image controls
+  var slideIndex = 1;
+  showSlides(slideIndex);
+
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("tripleCard");
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "flex";
+  }
+} else {
+  // Slideshow or Carousel
+  var slideIndex = 1;
+  showSlides(slideIndex);
+
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides((slideIndex += n));
+  }
+
+  // Thumbnail image controls
+  var slideIndex = 1;
+  showSlides(slideIndex);
+
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+  }
+}
